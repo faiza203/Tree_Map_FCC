c4bc07bd3114c8ad3ddf5293246df1cee0e175d9
faiza203 <fiazfaiza981@gmail.com>
2021-05-25 11:55:10 +0500

    Get Data

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log(" I am running ");
+// URLs
+const pledgesURL =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json";
+const movieURL =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json";
+const gameURL =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json";
+
+// Requirments
+let pledgesData, movieData, gameData;
+
+// GetPledgesData
+const getPledgesData = () => {
+  const req = new XMLHttpRequest();
+  req.open("get", pledgesURL, true);
+  req.onload = () => {
+    pledgesData = JSON.parse(req.responseText);
+  };
+  req.send();
+};
+
+// GetMovieData
+const getMovieData = () => {
+  const req = new XMLHttpRequest();
+  req.open("get", movieURL, true);
+  req.onload = () => {
+    movieData = JSON.parse(req.responseText);
+  };
+  req.send();
+};
+
+// GetGameData
+const getGameData = () => {
+  const req = new XMLHttpRequest();
+  req.open("get", gameURL, true);
+  req.onload = () => {
+    gameData = JSON.parse(req.responseText);
+  };
+  req.send();
+};
